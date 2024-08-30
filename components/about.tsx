9955adb211a009ac962f640855f1093283f4fc5e
Edison Lin <edisonlin@Edisons-MacBook-Pro.local>
2024-08-30 20:08:23 +0800

    first commit for website from vsc

---
+++ components/about.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import Image from "next/image";
+import back_photo from "@/public/back_photo.jpg"; // Adjust the path if needed
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-left leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+
+      {/* Image insertion */}
+      <div className="w-full mb-8">
+        <Image
+          src={back_photo}
+          alt="Background Photo"
+          layout="responsive"
+          width={700}
+          height={475}
+          className="rounded-lg"
+        />
+      </div>
+
+      <p className="mb-3">
+        Hi, I’m Edison, <span className="italic">a data science enthusiast</span> currently pursuing a <span className="font-bold">Bachelor of Science in Business Analytics with Honours at the National University of Singapore (NUS).</span> With a strong academic foundation and a <span className="font-bold">specialization in Machine Learning and Finance</span>, I’ve honed my skills in data management, visualization, and advanced analytics techniques.
+      </p>
+
+      <p className="mb-3">
+        My toolkit includes a variety of powerful data tools such as  <span className="font-bold">Python, R, MySQL, Tableau, Power BI, and MATLAB.</span> I’m adept at leveraging these technologies to uncover actionable insights from complex datasets, whether it’s through data-driven decision-making or innovative problem-solving.
+      </p>
+
+      <p>
+        Beyond my technical expertise, I enjoy mentoring students in mathematics, helping them achieve substantial grade improvements. As Sports Head at NUS Pioneer House, I lead initiatives that foster a vibrant sporting culture, enhancing participation through strategic planning and inclusive programs. I'm always eager to explore new challenges and opportunities where I can make a meaningful impact. Let’s connect and explore how we can collaborate!
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,117 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import { LuAperture } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+    {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Education Enrollment",
+    location: "National University of Singapore, Singapore",
+    description:
+      "With a strong passion for data analytics and building programs, I enrolled into NUS School of Computing, Business Analytics Programme, specialising in both Machine Learning and Finance",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023",
+  },
+  {
+    title: "Mathematics Tutor",
+    location: "The Impact Academy",
+    description:
+      "I worked as an Additional Mathematics and Elementary Mathematics Tutor where I sharpened my Mathematical Skill as well as learnt and implement various teaching techniques",
+    icon: React.createElement(CgWorkAlt),
+    date: "2023",
+  },
+  {
+    title: "Director of Sports",
+    location: "NUS Pioneer House",
+    description:
+      "I was appointed of Chief Sports Officer of NUS Pioneer House. I manage a team of 20 CCA Leaders and 15 Committee Members, to lead, manage and plan events",
+    icon: React.createElement(LuAperture),
+    date: "2024 - Present",
+  },
+  {
+    title: "Data Analyst Intern",
+    location: "KABAM Robotics",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2024 Summer",
+  },
+  
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "Java",
+  "Python",
+  "JavaScript",
+  "R",
+  "TableAu",
+  "PowerBI",
+  "MatLab",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "SQL",
+  "PostgreSQL",
+] as const;
